aa8da49adbdbd42c8981d5b51666fce65026153a
maharianto-dev <aridya.maharianto.dev@gmail.com>
2024-11-20 09:05:42 +0700

    upgrade to angular 19

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
   imports: [NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
