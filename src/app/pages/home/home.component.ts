60e80a8726fd3b1a16c7071af128ebb468597221
Eko Tristiyadi <eko.tristiyadi@qualysoft-coe.com>
2024-12-06 10:51:43 +0700

    add header, body, dan footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [FooterComponent, HeaderComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
